002718e701ee7a04ef6bc343ab156b26836b91ef
menachem m margolis <margolismendel5728@gmail.com>
2020-12-02 01:35:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(sentence)
   sentence = sentence.split
-  k = ""
-  sentence.collect do |a|
-  k+=" "+a.reverse
-
+  #k = ""
+  #sentence.collect do |a|
+  #k+=" "+a.reverse
+ sentence.split.collect {|word| word.reverse}.join(" ")
   end
 return k
 end
